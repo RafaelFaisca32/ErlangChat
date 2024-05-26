5c91f7df4b932b4b707954a940de1d6da239bf7b
Rafael Faisca <1180658@isep.ipp.pt>
2024-05-26 01:30:47 +0100

    ListClients need to move this to monitor

--- TheyChat1180658/server.erl
+++ TheyChat1180658/server.erl
@@ -11,26 +11,46 @@ get_host_name() ->
 init(Server) ->
     Router = list_to_atom("router@" ++ get_host_name()),
     createServer(Server,Router),
-    loop().
+    ListClients = [],
+    loop(ListClients).
+
+add_client(Client, ListClients) ->
+    case lists:member(Client, ListClients) of
+        true -> 
+            io:format("Not a new client: ~p~n", [Client]),
+            ListClients; % Client is already in the list, do not add
+        false -> 
+            io:format("Added a new client: ~p~n", [Client]),
+            [Client | ListClients] % Add new client to the list
+    end.
+
+remove_client(Client, ListClients) ->
+    io:format("Removed the client: ~p~n", [Client]),
+    lists:delete(Client, ListClients). % Remove client from the list
 
 createServer(Server, Router) ->
     net_adm:ping(Router),
     Pid = whereis(Server),  % Pid of the new Server
     erpc:call(Router, router, register_server, [Server, Pid]). % tcp calls to certain router methods
 
-loop() -> 
+loop(ListClients) -> 
     receive
         {From, stop} -> 
             io:format("Received from ~p message to stop!~n", [From]),
-            From ! {self(), server_disconnect};
+            UpdatedListClients = remove_client(From, ListClients),
+            io:format("CurrentClientList: ~p~n", [UpdatedListClients]),
+            From ! {self(), server_disconnect},
+            loop(UpdatedListClients);
         {From, Msg} -> 
+            UpdatedListClients = add_client(From, ListClients),
+            io:format("CurrentClientList~p~n", [UpdatedListClients]),
             io:format("Received ~p: ~p~n", [From, Msg]),
             io:format("Sending reply...~n"), 
             From ! {self(), happy_to_receive_your_message},
-            loop();
+            loop(UpdatedListClients);
         AnyOtherMsg ->
             io:format("Unknown message: ~p~n", [AnyOtherMsg]),
-            loop()
+            loop(ListClients)
     end.
 
 start_monitor(Server) ->
